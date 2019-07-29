edd9b8e8743bb3321f810b8bba73b53b26ceb702
Laura Radville <laura.radville@gmail.com>
2019-07-29 16:26:06 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,11 +3,17 @@ class Dog
   attr_accessor :owner, :mood
   attr_reader :name
   
+  @@all = []
+  
   def initialize(name, owner)
     @name = name
     @owner = owner
     @mood = "nervous"
+    @@all << self
   end
   
+  def self.all
+    @@all
+  end
 
 end
