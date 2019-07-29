288b891ae27b7e1babd79acf3faca410a5763c97
Laura Radville <laura.radville@gmail.com>
2019-07-29 16:20:32 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,9 +1,16 @@
 class Owner
   attr_reader :name, :species
+  
+  @@all = []
 
   def initialize(name)
     @name = name
     @species = "human"
+    @@all << self
+  end
+  
+  def self.all
+    
   end
   
   def say_species
